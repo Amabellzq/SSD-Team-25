81eb9f1e93868a68435e56a68e5f2174f4c19b44
Jun Wei <2202187@Sit.singaporetech.edu.sg>
2024-06-16 10:39:34 +0800

    Changes YML

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
+import logging
 from flask import Flask
 from .routes import main
 
+
 def create_app():
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
     app = Flask(__name__)
     app.register_blueprint(main)
+
+    logger.info("Flask application has been created successfully")
+
     return app
