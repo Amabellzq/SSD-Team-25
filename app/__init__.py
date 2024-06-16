123d4dafd868e2f2c3e1bea57999fb585ead482c
Jun Wei <2202187@Sit.singaporetech.edu.sg>
2024-06-16 10:34:05 +0800

    Static Page Testing 2

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,7 @@
 from flask import Flask
-from .config import Config
-from .extensions import db
 from .routes import main
-
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
-
-    db.init_app(app)
-
     app.register_blueprint(main)
-
     return app
