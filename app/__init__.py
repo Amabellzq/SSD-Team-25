0bc05d095d1e650a20a0d437ec8161096c4a3294
Jun Wei <2202187@Sit.singaporetech.edu.sg>
2024-06-16 17:47:50 +0800

    Gunicorn Troubleshooting 2

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,7 @@
-import logging
 from flask import Flask
 from .routes import main
 
-
 def create_app():
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger(__name__)
-
     app = Flask(__name__)
-
-    try:
-        app.register_blueprint(main)
-        logger.info("Flask application has been created successfully")
-    except Exception as e:
-        logger.error(f"Error creating Flask application: {e}")
-        raise e
-
+    app.register_blueprint(main)
     return app
