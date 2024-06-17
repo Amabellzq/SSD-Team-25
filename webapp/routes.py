ea2b26b791d42c3f2fb9e1cf614dcaf6dfe8385c
Jun Wei <2202187@Sit.singaporetech.edu.sg>
2024-06-17 14:40:00 +0800

    DB Testing

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,7 +1,34 @@
+# from flask import Blueprint, render_template
+#
+# main = Blueprint('main', __name__)
+#
+# @main.route('/')
+# def home():
+#     return render_template('homepage.html')
+
 from flask import Blueprint, render_template
+import pymysql
+import os
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
-def home():
-    return render_template('homepage.html')
+@main.route('/db_check')
+def db_check():
+    connection = None
+    try:
+        connection = pymysql.connect(
+            host=os.getenv('MYSQL_HOST'),
+            user=os.getenv('MYSQL_ADMIN_USER'),
+            password=os.getenv('MYSQL_ADMIN_PASSWORD'),
+            database=os.getenv('MYSQL_DATABASE'),
+            cursorclass=pymysql.cursors.DictCursor
+        )
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT DATABASE()")
+            db_name = cursor.fetchone()
+            return f"Connected to database: {db_name['DATABASE()']}"
+    except Exception as e:
+        return f"Error: {str(e)}"
+    finally:
+        if connection:
+            connection.close()
