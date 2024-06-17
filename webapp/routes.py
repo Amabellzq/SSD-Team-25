debfc1abf34aac0b16b5d7cf784f155973a7618e
Jun Wei <2202187@Sit.singaporetech.edu.sg>
2024-06-17 12:37:54 +0800

    Test Pages Change

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-    return "Welcome to the Homepage!"
+    return render_template('homepage.html')
