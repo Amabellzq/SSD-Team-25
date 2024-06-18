7341c0846eed2ae1f7ddb8ddae06129d86e1f708
Amabel <80372683+Amabellzq@users.noreply.github.com>
2024-06-18 17:23:59 +0800

    updated site GUI

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,36 @@
 # Use the official Python image from the Docker Hub
-FROM python:3.9-slim
+FROM python:3.8-slim
+
+# Install MySQL client dependencies and pkg-config
+RUN apt-get update && apt-get install -y \
+    default-libmysqlclient-dev \
+    build-essential \
+    pkg-config \
+    && rm -rf /var/lib/apt/lists/*
 
 # Set the working directory
-WORKDIR ./webapp
+WORKDIR /app
 
-# Copy the requirements file into the container
-COPY requirements.txt .
+# Copy the requirements file into the image
+COPY requirements.txt requirements.txt
 
 # Install the dependencies
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Copy the rest of the application code into the container
+# Copy the rest of the application code into the image
 COPY . .
 
-# Expose the port the webapp runs on
-EXPOSE 8000
+# Set environment variable to enable Gunicorn's live reload in development
+ENV GUNICORN_CMD_ARGS="--reload"
 
-# Run the application using Gunicorn with debug level logging
-CMD ["gunicorn", "--workers=3", "--bind=0.0.0.0:8000", "--log-level=debug", "webapp:app"]
+# Command to run the application
+CMD ["gunicorn", "-w", "3", "-b", "0.0.0.0:8000", "app:app"]
+
+#Command to install bootstrap for flask 
+RUN pip install Flask-Bootstrap
+
+#Command to install flask-login
+RUN pip install flask-login
+
+# Command to install WTF forms 
+RUN pip install Flask-WTF
---
+++ webapp/templates/includes/forms.py
@@ -0,0 +1,62 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, EmailField, SubmitField, BooleanField, SelectField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, Regexp
+
+#############################
+    #Authentication#
+#############################
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    contact = StringField('Contact Number', validators=[
+            DataRequired(),
+            Regexp(r'^\d{8}$', message="Contact number must be exactly 8 digits.")
+        ])   
+    role = SelectField('I am a', choices=[
+        ('user', 'User'),
+        ('seller', 'Seller')
+    ], validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=40)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message="Password must match")])
+    submit = SubmitField('Register')
+
+class ResetPasswordForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Reset Password')
+
+#############################
+    # Purchase #
+#############################
+
+class CheckoutForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    address2 = StringField('Apartment, suite, etc. (optional)')
+    postcode = StringField('Postcode/Zip', validators=[DataRequired()])
+    phone = StringField('Phone', validators=[DataRequired(), Length(min=10, max=10, message="Please enter a valid phone number.")])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Place Order')
+
+#############################
+    # Account #
+#############################
+
+class AccountDetailsForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=40)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message="Password must match")])
+    submit = SubmitField('Update Details')
+
+
+#############################
+    # Merchant #
+#############################
