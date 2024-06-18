cc8a1eb8693c38886541f7a6df6dc879ebb5c9b4
Jun Wei <khoojunwei6671@gmail.com>
2024-06-19 06:25:31 +0800

    Added DB Connection

---
+++ webapp/db.py
@@ -0,0 +1,12 @@
+import pymysql
+from flask import current_app
+
+def get_db_connection():
+    return pymysql.connect(
+        host=current_app.config['MYSQL_HOST'],
+        port=int(current_app.config['MYSQL_PORT']),
+        user=current_app.config['MYSQL_ADMIN_USER'],
+        password=current_app.config['MYSQL_ADMIN_PASSWORD'],
+        database=current_app.config['MYSQL_DATABASE'],
+        cursorclass=pymysql.cursors.DictCursor
+    )
