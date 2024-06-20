787b31765fc34e42edb1a60eb5c8b7bc551c23d6
Jun Wei <khoojunwei6671@gmail.com>
2024-06-20 13:08:04 +0800

    Database Test Fix

--- config.py
+++ config.py
@@ -7,12 +7,11 @@ load_dotenv()
 
 class Config:
     MYSQL_HOST = os.getenv('MYSQL_HOST')
-    MYSQL_PORT = os.getenv('MYSQL_PORT')
     MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
     MYSQL_ADMIN_USER = os.getenv('MYSQL_ADMIN_USER')
     MYSQL_ADMIN_PASSWORD = os.getenv('MYSQL_ADMIN_PASSWORD')
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{MYSQL_ADMIN_USER}:{MYSQL_ADMIN_PASSWORD}"
-        f"@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
+        f"@{MYSQL_HOST}/{MYSQL_DATABASE}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
