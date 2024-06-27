4bfa181521b40b89c57d38b9b6061ae6237cd2e7
Jun Wei <khoojunwei6671@gmail.com>
2024-06-27 18:59:22 +0800

    Config File Changes

--- config.py
+++ config.py
@@ -1,17 +1,26 @@
+from urllib.parse import quote_plus
+
 from dotenv import load_dotenv
 import os
-
+# is to to avoid circular imports
+from webapp import routes
 # Load environment variables from .env file
 load_dotenv()
 
 
 class Config:
+    # SECRET_KEY = os.getenv('SECRET_KEY', 'ssdT25')
     MYSQL_HOST = os.getenv('MYSQL_HOST')
+    MYSQL_PORT = os.getenv('MYSQL_PORT')
     MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
     MYSQL_ADMIN_USER = os.getenv('MYSQL_ADMIN_USER')
     MYSQL_ADMIN_PASSWORD = os.getenv('MYSQL_ADMIN_PASSWORD')
+
+    # URL-encode the username and password
+    MYSQL_ADMIN_USER_ENCODED = quote_plus(MYSQL_ADMIN_USER)
+    MYSQL_ADMIN_PASSWORD_ENCODED = quote_plus(MYSQL_ADMIN_PASSWORD)
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{MYSQL_ADMIN_USER}:{MYSQL_ADMIN_PASSWORD}"
-        f"@{MYSQL_HOST}/{MYSQL_DATABASE}"
+        f"mysql+pymysql://{MYSQL_ADMIN_USER_ENCODED}:{MYSQL_ADMIN_PASSWORD_ENCODED}"
+        f"@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
