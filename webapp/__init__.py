878a157e2889f6b143fda5b6c7f721b82b134cbe
Jun Wei <121539910+jwk-junwei@users.noreply.github.com>
2024-07-02 00:38:37 +0800

    Update __init__.py

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -16,7 +16,7 @@ app.config['DEBUG'] = True  # Enable debug mode
 # Configure session settings
 app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "sqlalchemy"
-app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
+app.config['SESSION_SQLALCHEMY'] = db
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=1)  # Session expires in 1 hour
 
 # Initialize SQLAlchemy
