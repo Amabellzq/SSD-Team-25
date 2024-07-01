2357b04424992acaa64306c32d6475c34f09bf98
lohzongyuan <56863053+lohzongyuan@users.noreply.github.com>
2024-07-01 21:33:49 +0800

    session expires in 1 hour

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -4,21 +4,22 @@ from .routes import main
 from flask_login import LoginManager
 from .models import db, User  # Import your user model
 from flask_session import Session
-
+from datetime import timedelta
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.config['SECRET_KEY'] = 'ssdT25'
 app.config['DEBUG'] = True  # Enable debug mode
+
 # Initialize SQLAlchemy
 db.init_app(app)
-app.config["SESSION_PERMANENT"] = False
+
+# Configure session settings
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "sqlalchemy"
 app.config['SESSION_SQLALCHEMY'] = db
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=1)  # Session expires in 1 hour
 Session(app)
-
-
-
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -26,10 +27,11 @@ login_manager.login_view = 'main.login'
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
+    return User.query.get(user_id)
 
 # Register Blueprint
 app.register_blueprint(main)
+
 # Optional: Uncomment if you want to create tables in a new setup
 # Create database tables
 # with app.app_context():
@@ -37,3 +39,6 @@ app.register_blueprint(main)
 
 # To ensure no circular import issues
 from .routes import main
+
+if __name__ == '__main__':
+    app.run(debug=True)
