9421f4a02e61c019425fedb89913394d0f05a5c8
Jun Wei <khoojunwei6671@gmail.com>
2024-07-06 02:35:19 +0800

    Test Again

--- webapp/tests/conftest.py
+++ webapp/tests/conftest.py
@@ -1,42 +1,30 @@
 import pytest
-from flask import Flask
-
-from webapp import db
+from webapp import app, db
 from config import TestConfig
-from webapp.model import User
 
 
 @pytest.fixture(scope='module')
 def test_client():
-    app = Flask(__name__)
+    # Configure the app for testing
     app.config.from_object(TestConfig)
-    # Ensure the SQLAlchemy instance is properly initialized
+
+    # Initialize the SQLAlchemy instance with the app
     db.init_app(app)
 
-    # Create a test client using the Flask application configured for testing
+    # Create a test client using the Flask application configured in your `__init__.py`
     with app.test_client() as testing_client:
         with app.app_context():
             # Create all tables
             db.create_all()
             yield testing_client  # this is where the testing happens
-
             # Drop all tables
             db.drop_all()
 
 
 @pytest.fixture(scope='module')
 def init_database():
-    # Create the database and the database table
-    db.create_all()
-
-    # Insert user data
-    user1 = User(username='testuser', email='test@example.com', password='password', role='Customer')
-    db.session.add(user1)
-
-    # Commit the changes for the users
-    db.session.commit()
-
-    yield db  # this is where the testing happens
-
-    db.session.remove()
-    db.drop_all()
+    # Create the database and the database table(s)
+    with app.app_context():
+        db.create_all()
+        yield db  # this is where the testing happens
+        db.drop_all()
