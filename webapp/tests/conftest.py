179495dde0d3f547c39f236e6590d874ca866330
Jun Wei <khoojunwei6671@gmail.com>
2024-07-06 21:35:11 +0800

    Selenium UI Shop Testing

--- webapp/tests/conftest.py
+++ webapp/tests/conftest.py
@@ -3,7 +3,10 @@ from flask import Flask
 
 from webapp import app , db
 from config import TestConfig
-
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope='module')
 def test_client():
@@ -22,3 +25,16 @@ def test_client():
 
             db.drop_all()
             db.session.remove()
+
+@pytest.fixture(scope="module")
+def driver():
+    # Set up the Chrome WebDriver with headless option
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+    driver.implicitly_wait(10)  # Implicit wait for 10 seconds
+    yield driver
+    driver.quit()
