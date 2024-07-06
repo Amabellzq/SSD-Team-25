9b91dd290c93d56e1894ec975cef2bbc8a864f92
Jun Wei <khoojunwei6671@gmail.com>
2024-07-06 10:49:13 +0800

    Test

--- webapp/tests/conftest.py
+++ webapp/tests/conftest.py
@@ -8,18 +8,18 @@ from config import TestConfig
 @pytest.fixture(scope='module')
 def test_client():
     # Configure the app for testing
+
     app.config.from_object(TestConfig)
-    # Ensure the db is only initialized once
-    with app.app_context():
-        db.init_app(app)
-        db.create_all()
 
     # Create a test client using the Flask application configured in your `__init__.py`
     with app.test_client() as testing_client:
         with app.app_context():
-            yield testing_client  # this is where the testing happens
+            # Create all tables
 
-    # Teardown phase
+            db.create_all()
+            yield testing_client  # this is where the testing happens
+            # Drop all tables
+            # Teardown phase
     with app.app_context():
         db.drop_all()
         db.session.remove()
