80aa919b876a4f1a2d0d5132ef38c5b41b7e5d10
Jun Wei <khoojunwei6671@gmail.com>
2024-07-08 15:08:57 +0800

    Test DB Control

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -6,7 +6,7 @@ from .routes import main
 from flask_login import LoginManager, current_user
 from .model import db, User
 from flask_wtf.csrf import CSRFProtect
-from .services import UserService 
+from .services import UserService
 from flask_session import Session
 from datetime import timedelta
 import base64
@@ -18,6 +18,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default-secret-key-if-none-found')
 app.config['DEBUG'] = True  # Enable debug mode
+
 
 def wait_for_db(host, port):
     retries = 5
@@ -35,10 +36,13 @@ def wait_for_db(host, port):
     if retries == 0:
         raise Exception("Database is not reachable. Exiting.")
 
+
 # Wait for the database to be ready
 wait_for_db('db', 3306)  # 'db' is the service name defined in docker-compose.yml
+
+
 # Function to dynamically select the database URI
-def get_db_uri(user_type= None):
+def get_db_uri(user_type=None):
     if user_type == 'Admin':
         return current_app.config['SQLALCHEMY_ADMIN_DATABASE_URI']
     elif user_type == 'Merchant':
@@ -49,6 +53,10 @@ def get_db_uri(user_type= None):
         return current_app.config['SQLALCHEMY_READONLY_DATABASE_URI']
     else:
         return current_app.config['SQLALCHEMY_READONLY_DATABASE_URI']
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = get_db_uri(None)  # Set to readonly as default
+
 # Initialize SQLAlchemy
 db.init_app(app)
 
@@ -67,11 +75,13 @@ login_manager.init_app(app)
 login_manager.login_view = 'main.login'
 
 # Initialize flask limiter
-limiter = Limiter(app = app, key_func=get_remote_address, default_limits=["100 per day", "25 per hour"])
+limiter = Limiter(app=app, key_func=get_remote_address, default_limits=["100 per day", "25 per hour"])
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return UserService.get(user_id)
+
 
 @app.before_request
 def before_request():
@@ -82,8 +92,10 @@ def before_request():
         db.engine.dispose()  # Dispose the current engine to reset the connection with the new URI
         db.init_app(app)
 
+
 # Register Blueprint
 app.register_blueprint(main)
+
 
 # Optional: Uncomment if you want to create tables in a new setup
 # Create database tables
@@ -93,6 +105,7 @@ app.register_blueprint(main)
 # Define the base64 encode filter
 def b64encode(value):
     return base64.b64encode(value).decode('utf-8')
+
 
 # Register the filter
 app.jinja_env.filters['b64encode'] = b64encode
