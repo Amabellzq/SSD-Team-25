99edcad6460381ec11c211726bae7714146dfe20
Jun Wei <khoojunwei6671@gmail.com>
2024-07-09 01:16:30 +0800

    Added Changes to Route.py

--- webapp/routes.py
+++ webapp/routes.py
@@ -34,7 +34,7 @@ limiter = Limiter(key_func=get_remote_address, default_limits=["100 per day", "2
 
 limiter.limit('25/hour')(main)
 def send_email(recipient_email, subject, body):
-
+    load_dotenv()
     OUTLOOK_EMAIL= os.getenv('OUTLOOK_EMAIL')
     OUTLOOK_PASSWORD= os.getenv('OUTLOOK_PASSWORD')
     smtp_server = 'smtp.outlook.com'
